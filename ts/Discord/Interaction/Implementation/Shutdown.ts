c32d7b4a858eb57acb6bca8ab4b518916016850a
Sergio Cabral <sergio@gohorse.dev>
2023-03-01 06:47:53 -0300

    feat: interação com botão para finalizar o bot

--- ts/Discord/Interaction/Implementation/Shutdown.ts
+++ ts/Discord/Interaction/Implementation/Shutdown.ts
@@ -2,11 +2,16 @@ import { InteractionHandler } from '../InteractionHandler';
 import {
   ActionRowBuilder,
   ButtonBuilder,
+  ButtonInteraction,
   ButtonStyle,
-  Interaction
+  ChatInputCommandInteraction,
+  Interaction,
+  MessageContextMenuCommandInteraction,
+  UserContextMenuCommandInteraction
 } from 'discord.js';
 import { TerminateApplication } from '@gohorse/npm-core';
 import { ICommandInteractionHandler } from '../ICommandInteractionHandler';
+import { NotImplementedError } from '@sergiocabral/helper';
 
 /**
  * Desliga o bot como aplição em execução no sistema operacional.
@@ -30,7 +35,7 @@ export class Shutdown
    * @param interaction Interação chegada do discord.
    */
   public canRun(interaction: Interaction): boolean {
-    return interaction.isCommand() && interaction.commandName === this.name;
+    return this.isStep1(interaction) || this.isStep2(interaction);
   }
 
   /**
@@ -38,31 +43,94 @@ export class Shutdown
    * @param interaction Interação chegada do discord.
    */
   public override async run(interaction: Interaction): Promise<void> {
-    if (!interaction.isCommand()) {
-      return;
+    if (this.isStep1(interaction)) {
+      await this.step1(interaction);
+    } else if (this.isStep2(interaction)) {
+      await this.step2(interaction);
+    } else {
+      throw new NotImplementedError(
+        `Invalid execution for interaction "${this.constructor.name}".`
+      );
     }
+  }
 
-    const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
-      new ButtonBuilder()
-        .setCustomId('no')
-        .setLabel('No. It was just a joke.'.translate())
-        .setStyle(ButtonStyle.Primary),
-      new ButtonBuilder()
-        .setCustomId('yes')
-        .setLabel('Yes. You deserve it.'.translate())
-        .setStyle(ButtonStyle.Danger)
+  /**
+   * Botáo Sim: Id
+   */
+  private buttonYesId = Math.random().toString();
+
+  /**
+   * Botáo Sim: Instância
+   */
+  private buttonYes: ButtonBuilder = new ButtonBuilder()
+    .setCustomId(this.buttonYesId)
+    .setLabel('Yes. You deserve it.'.translate())
+    .setStyle(ButtonStyle.Danger);
+
+  /**
+   * Botáo Não: Id
+   */
+  private buttonNoId = Math.random().toString();
+
+  /**
+   * Botáo Não: Instância
+   */
+  private buttonNo: ButtonBuilder = new ButtonBuilder()
+    .setCustomId(this.buttonNoId)
+    .setLabel('No. It was just a joke.'.translate())
+    .setStyle(ButtonStyle.Primary);
+
+  /**
+   * Passo 1: checagem.
+   */
+  private isStep1(
+    interaction: Interaction
+  ): interaction is
+    | ChatInputCommandInteraction
+    | MessageContextMenuCommandInteraction
+    | UserContextMenuCommandInteraction {
+    return interaction.isCommand() && interaction.commandName === this.name;
+  }
+
+  /**
+   * Passo 1: execução.
+   */
+  private async step1(interaction: Interaction): Promise<void> {
+    if (interaction.isCommand()) {
+      const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
+        this.buttonNo,
+        this.buttonYes
+      );
+
+      await interaction.reply({
+        content: 'You intend to kill me, is that it?'.translate(),
+        components: [row]
+      });
+    }
+  }
+
+  /**
+   * Passo 1: checagem.
+   */
+  private isStep2(interaction: Interaction): interaction is ButtonInteraction {
+    return Boolean(
+      interaction.isButton() &&
+        [this.buttonYesId, this.buttonNoId].includes(interaction.customId)
     );
+  }
 
-    await interaction.reply({
-      content: 'You intend to kill me, is that it?'.translate(),
-      components: [row]
-    });
-
-    return;
-
-    await new TerminateApplication(
-      this.configuration.applicationParameters.id,
-      this.configuration.applicationParameters.id
-    ).sendAsync();
+  /**
+   * Passo 2: execução.
+   */
+  private async step2(interaction: ButtonInteraction): Promise<void> {
+    if (interaction.customId === this.buttonNoId) {
+      await interaction.reply('Phew! What a fright.'.translate());
+    } else if (interaction.customId === this.buttonYesId) {
+      await interaction.reply("I'll be back.".translate());
+      await new TerminateApplication(
+        this.configuration.applicationParameters.id,
+        this.configuration.applicationParameters.id
+      ).sendAsync();
+    }
   }
 }
